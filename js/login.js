6e9a04757c5a4276bf4dd4a1d81e48e4446650ad
SRINAGESH18B <45843078+SRINAGESH17@users.noreply.github.com>
2021-08-24 22:59:21 +0530

    Profile page fetching data from Realtime Database
    Form on profile page is now able to get Data from Realtime Database

--- js/login.js
+++ js/login.js
@@ -1,14 +1,16 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyCSs4bnywJYZkrwqWEc1DT4N6VFZxmGW9c",
-  authDomain: "skygoal-login.firebaseapp.com",
-  projectId: "skygoal-login",
-  storageBucket: "skygoal-login.appspot.com",
-  messagingSenderId: "687400227804",
-  appId: "1:687400227804:web:1ec36fa8187d47194ef519",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  var firebaseConfig = {
+    apiKey: "AIzaSyCSs4bnywJYZkrwqWEc1DT4N6VFZxmGW9c",
+    authDomain: "skygoal-login.firebaseapp.com",
+    databaseURL:
+      "https://skygoal-login-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "skygoal-login",
+    storageBucket: "skygoal-login.appspot.com",
+    messagingSenderId: "687400227804",
+    appId: "1:687400227804:web:1ec36fa8187d47194ef519",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
 function signUp() {
@@ -35,13 +37,28 @@ function signIn() {
   const promise = auth.signInWithEmailAndPassword(
     lemail.value,
     lpassword.value
-  );
+  ).then(function (result) {
+    console.log(result);
+    window.location.href = "/index.html";
+  });
+
   promise.catch((e) => alert(e.message));
 }
 function signOut() {
-  auth.signOut();
-  alert("signed out");
-  document.getElementById("change").innerHTML = "Login";
+  auth.signOut().then(() => {
+  
+ 
+    console.log('signed out');
+    window.location.href = "/index.html";
+  });
+    // window.location.href = "/index.html";
+  // document.getElementById("change").innerHTML = "Login";
+
+   if (document.querySelector(".profile-btn") != null) {
+     document.querySelector(".profile-btn").style.display = "none";
+   }
+  
+
 }
 
 auth.onAuthStateChanged(function (user) {
@@ -50,8 +67,17 @@ auth.onAuthStateChanged(function (user) {
     // console.log(user);
 
     var lemail = user.email;
-    alert("Signed In " + lemail);
+    console.log("Signed in as" + lemail);
+    // alert("Signed In " + lemail);
     document.getElementById("login-btn").innerHTML = "Logout";
+
+    if (document.querySelector(".profile-btn") != null) {
+      document.querySelector(".profile-btn").style.display = "block";
+    }
+    document.getElementById("login-btn").addEventListener("click", function () {
+      signOut();
+
+    });
 
     if (document.querySelector(".sign-up") != null) {
       document.querySelector(".sign-up").style.display = "none";
@@ -98,4 +124,63 @@ function fbRedirect() {
     });
   // ...
   event.preventDefault();
+}
+  // Get a reference to the database service
+  var database = firebase.database();
+
+
+function getData(user) {
+
+  
+  //  gets data from realtime database if present and fills into the fields
+  const dbRef = firebase.database().ref();
+  dbRef
+    .child("users")
+    .child(user.uid)
+    .get()
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        // console.log(snapshot.val());
+        document.querySelector("#name").value = snapshot.val().name;
+        document.querySelector("#surname").value = snapshot.val().surname;
+        document.querySelector("#email").value = snapshot.val().email;
+        document.querySelector("#mobile").value = snapshot.val().mobile;
+      } else {
+        console.log("No data ");
+        fillDefault(user);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+
+
+
+
+function writeUserData(user) {
+  var name = document.querySelector("#name").value;
+  var email = document.querySelector("#email").value;
+  var surname = document.querySelector("#surname").value;
+  var mobile = document.querySelector("#mobile").value;
+  firebase
+    .database()
+    .ref("users/" + user.uid)
+    .set(
+      {
+        name: name,
+        surname: surname,
+        email: email,
+        mobile: mobile,
+      },
+      (error) => {
+        if (error) {
+          console.error(error);
+        } else {
+          alert("Data saved");
+          console.log("data saved successfully");
+        }
+      }
+    );
 }
